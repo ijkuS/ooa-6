6832ca9fd4fb0ef4047f9828c8540654df02713e
ijkuS <129561259+ijkuS@users.noreply.github.com>
2024-11-30 20:57:09 +0900

    Build: Connect productCard to product detail page

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { useNavigate } from 'react-router';
 
 export default function ProductCard({ product }) {
-	const { images, title, price, category, id } = product; // Destructure within the component
-
+	const { images, title, price, category, brand, id } = product; // Destructure within the component
+	const navigate = useNavigate();
+	const handleClick = (e) => {
+		e.preventDefault();
+		console.log(id);
+		navigate(`/products/${id}`, { state: { product } });
+	};
 	return (
-		<>
-			<Link href={`/products/${id}`} id={id}>
-				<div className='image__holder'>
-					<img src={images[0]} alt={title} />
-				</div>
-				<div className='text__holder'>
-					<p className='product-title'>{title}</p>
-					<p className='category'>{category}</p>
-					<p className='price'>{`${price} USD`}</p>
-				</div>
-			</Link>
-		</>
+		<div className='product-card' onClick={handleClick}>
+			<div className='image__holder'>
+				<img src={images[0]} alt={title} />
+			</div>
+			<div className='text__holder'>
+				<p className='product-title'>{title}</p>
+				<p className='category'>{brand}</p>
+				<p className='category'>{category}</p>
+				<p className='price'>{`${price} USD`}</p>
+			</div>
+		</div>
 	);
 }
--- src/pages/products/ProductDetailPage.jsx
+++ src/pages/products/ProductDetailPage.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useLocation } from 'react-router';
 
-export default function ProductDetailPage() {
+export default function ProductDetailPage({ product }) {
+	const {
+		state: {
+			product: { id, images, title },
+		},
+	} = useLocation();
 	return (
 		<div>
 			<h2>ProductDetailPage</h2>
