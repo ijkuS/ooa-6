9273ef63511e9c68bb811c5709970a514c486fd6
ijkuS <129561259+ijkuS@users.noreply.github.com>
2024-12-02 15:21:03 +0900

    Styling: Create Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Footer() {
+	return (
+		<footer>
+			<ul>
+				<li>© 2024 JoyWorks Co.,Ltd. All Rights Reserved</li>
+				<li>Guides Terms of Sale </li>
+				<li>Terms of Use</li>
+				<li>OOA Privacy Policy</li>
+				<li>Your Privacy Choices </li>
+			</ul>
+		</footer>
+	);
+}
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router';
 import Navbar from '../components/Navbar';
 import AuthContextProvider from '../contexts/AuthContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Footer from '../components/Footer';
 
 export default function Root() {
 	const queryClient = new QueryClient();
@@ -13,6 +14,7 @@ export default function Root() {
 				<main>
 					<Navbar />
 					<Outlet />
+					<Footer />
 				</main>
 			</AuthContextProvider>
 		</QueryClientProvider>
