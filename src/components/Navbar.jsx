419dfaa59c8aebee61c6b023dd7442bbf9020a7a
ijkuS <129561259+ijkuS@users.noreply.github.com>
2024-12-03 10:33:25 +0900

    Style: Update responsive sidemenu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router';
 import User from './User';
 import CartStatus from './CartStatus';
 import { useAuthContext } from '../contexts/AuthContext';
 import logo from '../../public/assets/ooa-images/ooa-logo.svg';
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import { AiOutlineMenu } from 'react-icons/ai';
+import SideNav from './SideNav';
 
 export default function Navbar() {
-	const { user, login, logout } = useAuthContext();
-	const [isMenuOpen, setIsMenuOpen] = useState();
-	const toggleMenu = () => {
-		setIsMenuOpen(!isMenuOpen);
+	const { user } = useAuthContext();
+
+	const openNav = () => {
+		document.getElementById('main-side-nav').style.width = '70%';
 	};
 
 	return (
@@ -18,62 +19,29 @@ export default function Navbar() {
 			<Link className='logo' to='/'>
 				<img src={logo} alt='ooa-logo' />
 			</Link>
-			<button
-				className={`menu-toggle-button ${isMenuOpen ? 'open' : ''}`}
-				onClick={toggleMenu}>
-				<AiOutlineMenu />
-			</button>
+			<menu className='navbar-menu'>
+				<ul>
+					<li> {user && <User user={user} />}</li>
 
-			<div className={`subwrapper ${isMenuOpen ? 'open' : ''}`}>
-				<menu>
-					<ul>
-						<button
-							className={`menu-close-button ${
-								isMenuOpen ? 'close' : ''
-							}`}
-							onClick={toggleMenu}>
-							<AiOutlineClose />
-						</button>
-						<li>
-							<Link className='button' to='/products/all'>
-								All Products
+					<li>
+						{user && (
+							<Link className='button' to='/cart'>
+								<CartStatus user={user} />
 							</Link>
-						</li>
-						<li>
-							{user && user.isAdmin && (
-								<Link
-									className='button'
-									to='/admin/dashboard'>
-									Admin
-								</Link>
-							)}
-						</li>
-						<li>
-							{user ? (
-								<button
-									onClick={logout}
-									className='button special'>
-									Logout
-								</button>
-							) : (
-								<button
-									onClick={login}
-									className='button special'>
-									Login
-								</button>
-							)}
-						</li>
-						<li> {user && <User user={user} />}</li>
-						<li>
-							{user && (
-								<Link className='button' to='/cart'>
-									<CartStatus user={user} />
-								</Link>
-							)}
-						</li>
-					</ul>
-				</menu>
-			</div>
+						)}
+					</li>
+					<li>
+						<button
+							className='sidemenu button'
+							onClick={openNav}>
+							<AiOutlineMenu />
+						</button>
+					</li>
+				</ul>
+				<div id='main-side-nav' className='side-nav'>
+					<SideNav />
+				</div>
+			</menu>
 		</nav>
 	);
 }
---
+++ src/components/SideNav.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { useAuthContext } from '../contexts/AuthContext';
+import { Link } from 'react-router';
+
+import { AiOutlineClose } from 'react-icons/ai';
+
+export default function SideNav() {
+	const { user, uid, login, logout } = useAuthContext();
+	const closeNav = () => {
+		document.getElementById('main-side-nav').style.width = '0';
+	};
+	return (
+		<ul>
+			<div className='general-area'>
+				<li>
+					<button
+						className='sidemenu close button'
+						onClick={closeNav}>
+						<AiOutlineClose />
+					</button>
+				</li>
+				<li>New Arrivals</li>
+				<li>
+					<Link className='button' to='/products/all'>
+						All Products
+					</Link>
+				</li>
+				<li>Brands</li>
+				<li>Category</li>
+			</div>
+
+			<div className='auth-area'>
+				<li>
+					{user && user.isAdmin && (
+						<Link className='button' to='/admin/dashboard'>
+							Admin
+						</Link>
+					)}
+				</li>
+				<li>
+					{user ? (
+						<button
+							onClick={logout}
+							className='button special'>
+							Logout
+						</button>
+					) : (
+						<button
+							onClick={login}
+							className='button special'>
+							Login
+						</button>
+					)}
+				</li>
+			</div>
+		</ul>
+	);
+}
