130adac07b5aea84d0733c74daee971352755e2b
ijkuS <129561259+ijkuS@users.noreply.github.com>
2024-12-10 13:44:51 +0900

    Chore: Update for gh-page

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
 	mode: process.env.mode,
 	entry: './src/index.js',
 	output: {
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, 'build'),
 		filename: '[name].[fullhash].js',
 		publicPath: '/',
 	},
